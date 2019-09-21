1670d29f325dd54c75a3fd2da39d5965739013b3
ahtrahdis7 <44672399+ahtrahdis7@users.noreply.github.com>
2019-09-21 19:25:58 +0530

    Update test_mail.py

--- test_mail.py
+++ test_mail.py
@@ -6,15 +6,15 @@ mail=Mail(app)
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'sidharthabiki@gmail.com'
-app.config['MAIL_PASSWORD'] = 'sidhartha7'
+app.config['MAIL_USERNAME'] = 'xxxxx@gmail.com'
+app.config['MAIL_PASSWORD'] = 'hatt teri maa ki chuu'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 @app.route("/")
 def index():
-   msg = Message('Hello', sender = 'sidharthabiki@gmail.com', recipients = [''])
+   msg = Message('Hello', sender = 'nikal_lawde@chutmail.com', recipients = [''])
    msg.body = ""
    mail.send(msg)
    return "Sent"
